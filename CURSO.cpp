7c05fc956cf0d1cdfba2378370a9eb88d708fd8e
Cristian Infante <71674005+Cristian-Infante@users.noreply.github.com>
2022-02-09 22:08:23 -0500

    Update CURSO.cpp

--- CURSO.cpp
+++ CURSO.cpp
@@ -15,7 +15,7 @@ CURSO<T>::~CURSO(){
         aux->~ESTUDIANTE();
     }
     inicio = nullptr;
-    courseName = nullptr;
+    courseName = 0;
     siguiente = nullptr;
 }
 
@@ -28,6 +28,9 @@ template <typename T>
 bool CURSO<T>::isEmpty(){
     if(inicio == nullptr){
         return true;
+    }
+    else{
+        return false;
     }
 }
 
@@ -57,7 +60,7 @@ ESTUDIANTE<T> *CURSO<T>::getInicio(){
 }
 
 template <typename T>
-void CURSO<T>::pushEst(T est){
+void CURSO<T>::pushEstu(T est){
     ESTUDIANTE<T> *nuevo = new ESTUDIANTE<T>(est);
     if(isEmpty()){
         inicio = nuevo;
@@ -73,10 +76,107 @@ void CURSO<T>::pushEst(T est){
             if(act == nullptr){
                 ant->setSig(nuevo);
             }
-            else
+            else{
+                if(ant == nullptr){
+                    nuevo->setSig(inicio);
+                    inicio = nuevo;
+                }
+                else{
+                    nuevo->setSig(act);
+                    ant->setSig(nuevo);
+                }
+            }
         }
     }
 }
 
+template <typename T>
+void CURSO<T>::insertar(ESTUDIANTE<T> *est, ESTUDIANTE<T> *aux, ESTUDIANTE<T> *ant){
+    if(isEmpty()){
+        inicio = est;
+    }
+    else{
+        if((aux->getSig() != nullptr) && (aux->getEst() < est->getEst())){
+            ant = aux;
+            insertar(est,aux->getSig(),ant);
+        }
+        else{
+            if(aux->getEst() != est->getEst()){
+                if(aux == nullptr){
+                    ant->setSig(est);
+                }
+                else{
+                    if(ant == nullptr){
+                        est->setSig(inicio);
+                        inicio = est;
+                    }
+                    else{
+                        est->setSig(aux);
+                        ant->setSig(est);
+                    }
+                }
+            }
+        }
+    }
+}
 
+template <typename T>
+T CURSO<T>::popEst(T est){
+    if(isEmpty()){
+        //cout<"Is empty"<<endl;
+    }
+    else{
+        ESTUDIANTE<T> *act = inicio;
+        ESTUDIANTE<T> *ant = nullptr;
+        while((act->getSig() != nullptr) && (act->getEst() < est)){
+            ant = act;
+            act = act->getSig();
+        }
+        if(act->getEst() == est){
+            if(ant == nullptr){
+                inicio = inicio->getSig();
+                return act;
+            }
+            else{
+                ant->setSig(act->getSig());
+                return act;
+            }
+        }
+        else{
+            //cout<<"No existe"<<endl;
+        }
+    }
+}
 
+template <typename T>
+void CURSO<T>::print(){
+    ESTUDIANTE<T> *act = inicio;
+    while(act->getSig() != nullptr){
+        cout<<act->getEst()<<" ";
+        act = act->getSig();
+    }
+}
+
+template <typename T>
+void CURSO<T>::mostrar(ESTUDIANTE<T> *aux){
+    if(aux != nullptr){
+        aux->print();
+        mostrar(aux->getSig());
+    }
+}
+
+template <typename T>
+void CURSO<T>::queHago(ESTUDIANTE<T> *aux){
+    if(aux != nullptr){
+        if(aux->getEst() >= 1){
+            if(aux->getEst() % 3 != 0){
+                cout<<aux->getEst()<<" ";
+                queHago(aux->getSig());
+            }
+            else if(aux != nullptr){
+                queHago(aux->getSig());
+                cout<<aux->getEst()<<" ";
+            }
+        }
+    }
+}

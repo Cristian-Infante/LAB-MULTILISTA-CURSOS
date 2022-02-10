c930a0b50e980c64bd8a81ca686a161577e6feb9
Cristian Infante <71674005+Cristian-Infante@users.noreply.github.com>
2022-02-09 22:10:30 -0500

    Update MULTILISTA.h

--- MULTILISTA.h
+++ MULTILISTA.h
@@ -11,8 +11,17 @@ class MULTILISTA{
         virtual ~MULTILISTA();
         void setHead(CURSO<T> *);
         CURSO<T> *getHead();
+        bool isEmpty();
         void pushCourse(T);
         T popCourse(T);
+        void pushEs(T);
+        T popEs(T);
+        void pushEst(T, T);
+        T popEst(T, T);
+        void print();
+        void mostrar(CURSO<T> *);
+        bool buscar(CURSO<T> *, CURSO<T> *);
+
 };
 
 #endif // MULTILISTA_H
